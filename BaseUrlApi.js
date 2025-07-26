de9df1710e10b730de92abb2370608f0e2cf76c9
Faizurcse <mdfaizurrhman2000@gmail.com>
2025-07-26 22:50:24 +0530

    Resolve merge conflict in BaseUrlApi.js - fix syntax error and restore comment

--- BaseUrlApi.js
+++ BaseUrlApi.js
@@ -1,3 +1,4 @@
 // Centralized API base URL
-const BASE_API_URL = https://ats-backend-nodejs.onrender.com/api || "http://localhost:5000/api"
+const BASE_API_URL = "https://ats-backend-nodejs.onrender.com/api" || "http://localhost:5000/api";
 export default BASE_API_URL; 
+
